31276a340f463837c76f192fd3e6695f002778fc
meedoox <matysek.herman@gmail.com>
2024-11-16 23:38:46 +0100

    refactor: remove unnecesary code

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -4,25 +4,22 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <Card>
       <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
+        <CardTitle>Create Alert</CardTitle>
+        <CardDescription>
+          Do you have something on your mind? You can tell us ❤️
+        </CardDescription>
       </CardHeader>
       <CardContent>
         <AlertForm />
       </CardContent>
-      <CardFooter className='flex justify-end'>
-        <Button>Deploy</Button>
-      </CardFooter>
     </Card>
   )
 }
